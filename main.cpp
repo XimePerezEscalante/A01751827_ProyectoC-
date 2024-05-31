10557eb861dab001357dbc0556a59509a1174dfd
XimePerezEscalante <142125448+XimePerezEscalante@users.noreply.github.com>
2024-05-31 01:22:39 -0600

    Update main.cpp
    Se cambió la creación de los sobrevivientes y ahora se usan apuntadores

--- main.cpp
+++ main.cpp
@@ -38,31 +38,59 @@ string nombreSob(int select){
 
 int main(){
   int opcion;
-  string name, user;
-
-  cout << "LEFT 4 DEAD (versión barata)\nContexto: En tu equipo habrá cuatro sobrevivientes, haz lo mejor posible para completar el nivel.\n¿Cuál es tu nombre?\n" << endl;
-  cin >> user;
-  cout << "¿Quieres jugar con los personajes del primer o segundo juego? (Escribe 1 o 2)\n " << endl;
-
-  cin >> opcion;
-  if (opcion == 1){
+    string name, user;
+    Jugador * equipo[4];
+    
+    cout << "LEFT 4 DEAD (versión barata)\nContexto: En tu equipo habrá cuatro sobrevivientes, haz lo mejor posible para completar el nivel.\n" << endl;
+    user = "Usuario";
+    
+    cout << "¿Quieres jugar con los personajes del primer o segundo juego? (Escribe 1 o 2)\n " << endl;
+    cin >> opcion;
+    
+    if (opcion == 1){
+        Jugador *zoey = new Sobreviviente("Zoey (Bot)");
+        Jugador *bill = new Sobreviviente("Bill (Bot)");
+        Jugador *louis = new Sobreviviente("Louis (Bot)");
+        Jugador *frank = new Sobreviviente("Frank (Bot)");
+        equipo[0] = bill;
+        equipo[1] = zoey;
+        equipo[2] = frank;
+        equipo[3] = louis;
         cout << "Escoge un personaje:\n1. Bill\n2. Zoey\n3. Frank\n4. Louis\n" << endl;
         cin >> opcion;
         name = nombreSob(opcion);
-  }
-  else{
+    }
+        
+    else{
+        Jugador *rochelle = new Sobreviviente("Rochelle (Bot)");
+        Jugador *nick = new Sobreviviente("Nick (Bot)");
+        Jugador *coach = new Sobreviviente("Coach (Bot)");
+        Jugador *ellis = new Sobreviviente("Ellis (Bot)");
+        equipo[0] = ellis;
+        equipo[1] = rochelle;
+        equipo[2] = coach;
+        equipo[3] = nick;
         cout << "Escoge un personaje:\n1. Ellis\n2. Rochelle\n3. Coach\n4. Nick" << endl;
         cin >> opcion;
         name = nombreSob(opcion+4);
-  }
-
-  Sobreviviente usuario(user + " (" + name + ")",100.0);
+    }
     
-  //Pruebas para ver que funcione el programa
-  cout << usuario.getNombre() << endl;
+    Jugador *usuario = new Sobreviviente(user + " (" + name + ")");
+    equipo[opcion-1] = usuario;
+    
+    
+    cout << "EQUIPO:" << endl;
+    
+    for (int i = 0;i < 4;i++){
+        cout << equipo[i]->getNombre() << endl;
+    }
 
-  Infectado inf1;
-
-  usuario.ataque(inf1);
-  inf1.ataque(usuario);
+    //Pruebas para ver que funcione el programa
+    Jugador *inf1 = new Infectado();
+    Jugador *tank = new InfectadoEspecial("Lanzar objetos y jugadores","Tank",3000.0);
+    inf1->ataque(usuario);
+    tank->ataque(equipo[0]);
+    
+    usuario->ataque(inf1);
+    usuario->ataque(tank);
 }
