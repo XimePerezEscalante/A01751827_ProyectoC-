809a6675fa160d60190f3d60be6e1249acb9f08a
XimePerezEscalante <142125448+XimePerezEscalante@users.noreply.github.com>
2024-06-14 05:07:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
  * A01751827
  * 14/06/2024
  *
- * En este proyecto se simula una partida de Left 4 Dead, en el que hay 4 sobrevivientes
- * que deben combatir a una horda de infectados (zombies) e infectados especiales
- * (mutaciones). En ella el usuario puede escoger si jugar con los sobrevivientes del 
- * primer o segundo juego, además de interactuar disparándole a los infectados o curando
+ * En este proyecto se simula una partida de Left 4 Dead, en el que 
+ * hay 4 sobrevivientes que deben combatir a una horda de infectados 
+ * (zombies) e infectados especiales (mutaciones). En ella el usuario
+ * puede escoger si jugar con los sobrevivientes del primer o segundo 
+ * juego, además de interactuar disparándole a los infectados o curando
  * a sus compañeros hasta que este muera.
  */
 
@@ -23,12 +24,14 @@ int main(int argc, const char * argv[]) {
     string user;
     Mapa m1;
     
-    cout << "LEFT 4 DEAD (versión barata)\nContexto: En tu equipo habrá cuatro sobrevivientes,
-        haz lo mejor posible para completar el nivel.\n¿Cuál es tu nombre?\n " << endl;
+    cout << "LEFT 4 DEAD (versión barata)\n
+        Contexto: En tu equipo habrá cuatro sobrevivientes,
+        haz lo mejor posible para completar el nivel.\n¿Cuál es tu nombre?"
+        << endl;
     cin >> user;
     
     cout << "¿Quieres jugar con los personajes del primer o segundo juego? 
-        (Escribe 1 o 2)\n " << endl;
+        (Escribe 1 o 2)" << endl;
     cin >> opcion;
     
     while (opcion != 1 && opcion !=2){
@@ -39,12 +42,14 @@ int main(int argc, const char * argv[]) {
     m1.creaEquipo(opcion);
     
     if (opcion == 1){
-        cout << "Escoge un personaje:\n1. Zoey\n2. Bill\n3. Louis\n4. Frank\n" << endl;
+        cout << "Escoge un personaje:\n1. Zoey\n2. Bill\n3. Louis\n
+            4. Frank\n" << endl;
         cin >> opcion;
     }
     
     else{
-        cout << "Escoge un personaje:\n1. Rochelle\n2. Nick\n3. Coach\n4. Ellis" << endl;
+        cout << "Escoge un personaje:\n1. Rochelle\n2. Nick\n3. Coach\n
+            4. Ellis" << endl;
         cin >> opcion;
     }
     
